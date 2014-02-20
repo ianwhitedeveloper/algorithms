cbc8cddd77a107d497977722fe7369fd70fd2d8f
Ian White <ianwhite@utexas.edu>
2014-02-20 14:02:17 -0600

    save before start of lesson

--- hash.rb
+++ hash.rb
@@ -9,13 +9,19 @@ class MyHash
 
   def [](key)
     pair = @array[find_index(key)]
-    if pair.first == key
+    if pair
       pair.last
     end
   end
 
    def []=(key, value)
-    @array[find_index(key)] = [[key,value]]
-    value
+    @array[find_index(key)].each do |x|
+     if x.nil?
+      x = [[key,value]]
+      value
+    else
+      x = [[value]]
+    end
+  end
   end
 end
