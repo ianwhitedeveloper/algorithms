c0b131c2d8578b59fe1b20174d15c240e4f16da2
Ian White <ianwhite@utexas.edu>
2014-03-04 13:56:33 -0600

    use Shehzans selection sort alogrithm

--- sort/sort.rb
+++ sort/sort.rb
@@ -1,7 +1,22 @@
 module Sort
-
   def self.selection_sort(array)
-    array.sort
+    array.each_index do |x|
+      shift_nth_smallest(x, array)
+    end
+    array
   end
 
+  def self.shift_nth_smallest(i, array)
+    min = array[i]
+    min_index = i
+    for x in i..(array.size - 1)
+      if min > array[x]
+        min = array[x]
+        min_index = x
+      end
+    end
+
+    array[min_index] = array[i]
+    array[i] = min
+  end
 end
